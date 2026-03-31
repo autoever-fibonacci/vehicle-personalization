4ac000b9fe41ecab3a3921e26763f395a72c6f10
gahyunparkk <gahyun030412@gmail.com>
2026-03-31 15:35:20 +0900

    feat: RFID ID 인식

--- Ecu_Access_Body_TC275/AppSw/RC522_Driver.h
+++ Ecu_Access_Body_TC275/AppSw/RC522_Driver.h
@@ -7,7 +7,7 @@
 #define PICC_ANTICOLL  0x93
 #define STATUS_OK      0x00
 
-void RC522_Init(void);
+void  RC522_Init(void);
 uint8 RC522_Request(uint8 reqMode, uint8 *TagType);
 uint8 RC522_Anticoll(uint8 *serNum);
 
